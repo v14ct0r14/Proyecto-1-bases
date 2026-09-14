fed45df53e90e0c7770a3d30dd6718bd3b641359
Victoria :P <victoriaalvri@gmail.com>
2026-09-13 22:41:56 -0600

    Update agenda.py
    corrí el botón para que los demás se puedan ver

--- agenda.py
+++ agenda.py
@@ -146,7 +146,7 @@ class AppAgenda(ctk.CTk):
             self.sidebar_frame,
             text="🔄  Recargar datos",
             command=self.actualizar_todas_las_tablas
-        ).grid(row=5, column=0, padx=15, pady=(20, 5), sticky="ew")
+        ).grid(row=9, column=0, padx=15, pady=(12, 3), sticky="ew")
 
         ctk.CTkLabel(self.sidebar_frame, text="APARIENCIA", font=ctk.CTkFont(size=11, weight="bold")).grid(
             row=11, column=0, padx=20, pady=(10, 5), sticky="w"
